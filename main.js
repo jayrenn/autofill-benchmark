4c59e1b1582e5e418dea9e51b2050e14df305668
jayrenn <joshrenn@microsoft.com>
2016-01-15 10:44:58 -0800

    make state an input

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+var baselineContactSettings = [
+  { id: "firstName", name: "firstName", placeholder: "Double click here" },
+  { id: "middleName", name: "middleName" },
+  { id: "lastName", name: "lastName" },
+  { id: "addressLine1", name: "addressLine1" },
+  { id: "addressLine2", name: "addressLine2" },
+  { id: "city", name: "city" },
+  { },
+  {},
+  {},
+  {},
+  {},
+];
+
 var firstNameSettings = [
   { id: "first-name" },
   { name: "fname" }
@@ -8,7 +22,7 @@ function createFieldInput(settings, stub, type) {
   field.className = "field";
   var label = document.createElement("label");
   var input = document.createElement("input");
-  input.type = type;
+  input.type = type || "text";
   var spacer = document.createTextNode("\n");
   var settingsKeys = Object.keys(settings);
 
@@ -27,10 +41,12 @@ function createFieldInput(settings, stub, type) {
 function init() {
 	var stub;
 
+  // Baseline for contact information
+
 	// First name permutations
 	stub = document.getElementById("firstNamePermutations");
   for (var i = 0; i < firstNameSettings.length; i++) {
-    createFieldInput(firstNameSettings[i], stub, "text");
+    createFieldInput(firstNameSettings[i], stub);
   }
 }
 
